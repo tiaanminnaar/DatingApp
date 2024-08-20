d30a9a0adca966815abda1cedfd534c3afd1a1a9
Tiaan Minnaar <tiaanminnaar7@gmail.com>
2024-08-20 14:30:20 +0200

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UsersController(DataContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync()
+        {
+            var users = await context.AppUsers.ToListAsync();
+
+            return users;
+        }
+        [HttpGet("{id : int}")]
+        public async Task<ActionResult<AppUser>> GetUserAsync(int id)
+        {
+            var user = await context.AppUsers.FindAsync(id);
+
+            if (user == null) return NotFound();
+
+            return user;
+        }
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace API.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
