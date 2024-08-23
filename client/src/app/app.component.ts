0cba34d0eb741e3223e87e27eb422df630f8595c
Tiaan Minnaar <tiaanminnaar7@gmail.com>
2024-08-23 09:38:45 +0200

    section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,13 +14,10 @@ import { HomeComponent } from './home/home.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
-  private accountService = inject(AccountService)
-  title = 'Dating App';
-  users: any;
+  [x: string]: any;
+  private accountService = inject(AccountService);
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -29,13 +26,5 @@ export class AppComponent implements OnInit {
     if (!userString) return;
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:7070/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
   }
 }
