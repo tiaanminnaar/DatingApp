1e3896f3cf9cb9597ae00f2178484071607f7ce2
Tiaan Minnaar <tiaanminnaar7@gmail.com>
2024-08-29 11:32:19 +0200

    chapter 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,8 +1,19 @@
-﻿namespace API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDto
 {
-    public class RegisterDto
-    {
-        public required  string Username { get; set; }
-        public required string Password { get; set; }
-    }
+    [Required]
+    public string Username { get; set; } = string.Empty;
+
+    [Required] public string? KnownAs { get; set; }
+    [Required] public string? Gender { get; set; }
+    [Required] public string? DateOfBirth { get; set; }
+    [Required] public string? City { get; set; }
+    [Required] public string? Country { get; set; }
+
+    [Required]
+    [StringLength(8, MinimumLength = 4)]
+    public string Password { get; set; } = string.Empty;
 }
